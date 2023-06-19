2a5c9a3d856f79a4d954b910d2266766404ceaf8
dekzyd <adekoyadayo2010@gmail.com>
2023-06-19 10:25:51 +0100

    change cartItem design

--- src/CartContainer.jsx
+++ src/CartContainer.jsx
@@ -12,7 +12,7 @@ const CartContainer = () => {
       <section className="cart">
         {/* cart header */}
         <header>
-          <h2>your bag</h2>
+          <h2>your cart</h2>
           <h4 className="empty-cart">is currently empty</h4>
         </header>
       </section>
